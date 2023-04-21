86b307344d62e5ed62a46887a3e80a95fbb80f4a
Chris <chris.r.lee43@gmail.com>
2023-04-20 20:26:20 -0700

    boilerplate

---
+++ src/lib/Alert.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let alertText: string = "";
+  export let doubleAlertText: string = "";
+  alert("womp diggity");
+  const customAlert = () => {
+    alert(alertText);
+    alert(doubleAlertText);
+  };
+</script>
+
+<button on:click={customAlert}> Show me </button>
