eea5dd2f4e953cc908ba14425dbf4e9305ce1203
Chris <chris.r.lee43@gmail.com>
2023-05-23 19:34:26 -0700

    restructure and support background page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "popup.html"),
+        background: path.resolve(__dirname, "src/background/main.ts"),
+      },
+      output: {
+        dir: "dist",
+        format: "esm",
+        entryFileNames: "js/[name].js",
+        chunkFileNames: "js/chunks/[name]-[hash].js",
+      },
+    },
+  },
 });
